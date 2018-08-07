5088ea8e3640a731bcda64c4ae888e48b8899ca1
KZW <k.a.zwierzynski@gmail.com>
2018-08-07 15:31:07 +0200

    added tasks in gulp for .js and .html

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
-// Compile sass into CSS & auto-inject into browsers
+// Compile sass into CSS + add prefixes & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
@@ -16,22 +16,30 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-// // Move the javascript files into our /src/js folder
-// gulp.task('js', function() {
-//     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-//         .pipe(gulp.dest("src/js"))
-//         .pipe(browserSync.stream());
-// });
+// Move the html files into our /dist folder
+gulp.task('html', function() {
+    return gulp.src(['src/*.html'])
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
+});
+
+// Move the js files into our /dist folder
+gulp.task('js', function() {
+    return gulp.src(['src/js/*.js'])
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['html', 'js', 'sass'], function() {
 
     browserSync.init({
         server: "./src"  
     });
 
     gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['src/js/*.js'], ['js']);
+    gulp.watch(["src/*.html"],['html']).on('change', browserSync.reload);
     // gulp.watch("src/js/*.js").on('change', browserSync.reload);
 });
 
