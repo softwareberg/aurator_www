cc953d05d874cd203e384ad5a2eae16e0b45c8a0
KZW <k.a.zwierzynski@gmail.com>
2018-09-05 10:23:42 +0200

    feature/firma pre PR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
-import { setHeights, resetHeights, setPositionForeground } from 'js/layout';
+import {
+  setHeightsDesktop, setHeightsAll, resetHeights, setFotoHeight
+} from 'js/layout';
 
 require('css/main.scss');
 
@@ -9,29 +11,35 @@ const breakpointDesktop = 992;
 const yearFounded = 2008;
 
 $(document).ready(() => {
-  const yearNow = (new Date()).getFullYear();
-  $('.js-doswiadczenie__x-lat').html(`${yearNow - yearFounded} lat doświadczenia`);
   $('.top').css('height', window.innerHeight);
-  setTimeout(() => setPositionForeground(), 500);
-  // setPositionFoto();
+  setTimeout(() => setFotoHeight(), 500);
+  const yearNow = (new Date()).getFullYear();
+  // set experience time under the logo in 'Doswiadczenie' section
+  $('.js-doswiadczenie__x-lat').html(`${yearNow - yearFounded} lat doświadczenia`);
   $('.menu-toggle').click(() => {
     $('.nav-mobile').toggleClass('h-open');
     $('.menu-toggle').toggleClass('h-open');
   });
 
+  // for development purposes only:
+  setTimeout(() => setHeightsAll(), 500);
+  // for production purposes:
+  // setHeightsAll();
+
   if (window.innerWidth >= breakpointDesktop) {
     // for development purposes only:
-    setTimeout(() => setHeights(), 500);
+    setTimeout(() => setHeightsDesktop(), 500);
     // for production purposes:
-    // setHeights();
+    // setHeightsDesktop();
   }
-  window.addEventListener('resize', () => {
+
+  $(window).resize(() => {
     $('.top').css('height', window.innerHeight);
-    setPositionForeground();
-    // setPositionFoto();
+    setFotoHeight();
     if (window.innerWidth >= breakpointDesktop) {
       // on resize of a window adjust heights of mirrored elements
-      setHeights();
+      setHeightsDesktop();
+      setHeightsAll();
       // on resize of a window reset state of mobile menu
       $('.nav-mobile').removeClass('h-open');
       $('.menu-toggle').removeClass('h-open');
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,40 +1,52 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-const setHeightsArray = [
-  // { className: 'flex-col--2__h1', parentId: 'naprawa'},
-  // { className: 'flex-col--2__p--1', parentId: 'naprawa'},
-  // { className: 'flex-col--2__p--2', parentId: 'naprawa'},
-  // { className: 'flex-col--2__list', parentId: 'naprawa'},
+const setHeightsArrayDesktop = [
+  { elementClass: 'section__header', parentId: 'naprawa' },
+  { elementClass: 'section__p--1', parentId: 'naprawa' },
+  { elementClass: 'section__p--2', parentId: 'naprawa' },
+  { elementClass: 'section__list-div', parentId: 'naprawa' },
+  { elementClass: 'section__header', parentId: 'produkcja' },
+  { elementClass: 'section__p--1', parentId: 'produkcja' },
+  { elementClass: 'section__p--2', parentId: 'produkcja' },
+  { elementClass: 'section__list-div', parentId: 'produkcja' }
 ];
 
+const setHeightsArrayAll = [
+  { elementClass: 'section__p--3', parentId: 'produkcja' }
+];
 
 // adjust height of a div to the highest child of all considered divs
-export function setHeights() {
-  setHeightsArray.forEach((arrayEl) => {
-    const elements = $(`#${arrayEl.parentId}`).find(`.${arrayEl.className}`).toArray();
-    const heights = elements.map(it => it.scrollHeight);
-    const maxHeight = _.max(heights);
-    elements.forEach((el) => { el.style.height = `${maxHeight}px`; });
-  });
+function setHeightsInner(elementsWithParents) {
+  return () => {
+    elementsWithParents.forEach((elementWithParent) => {
+      const elements = $(`#${elementWithParent.parentId}`).find(`.${elementWithParent.elementClass}`).toArray();
+      const heights = elements.map(it => it.children[0].scrollHeight);
+      const maxHeight = _.max(heights);
+      elements.forEach((el) => { el.style.height = `${maxHeight}px`; });
+    });
+  };
 }
+
+// for desktop widths
+const setHeightsDesktop = setHeightsInner(setHeightsArrayDesktop);
+// for all widths
+const setHeightsAll = setHeightsInner(setHeightsArrayAll);
+
+export { setHeightsDesktop, setHeightsAll };
 
 // reset heights of divs to heights of their own children
 export function resetHeights() {
-  setHeightsArray.forEach((arrayEl) => {
-    const elements = $(`#${arrayEl.parentId}`).find(`.${arrayEl.className}`).toArray();
+  setHeightsArrayDesktop.forEach((elementWithParent) => {
+    const elements = $(`#${elementWithParent.parentId}`).find(`.${elementWithParent.elementClass}`).toArray();
     elements.forEach((el) => { el.style.height = ''; });
   });
 }
 
-export function setPositionForeground() {
+export function setFotoHeight() {
   const quoteHeight = $('.firma__quote-div').height();
   const foregroundHeight = $('.firma__foreground').height();
   const foto = $('.firma__foto');
   const fotoHeight = quoteHeight + foregroundHeight;
   foto.height(fotoHeight);
-  // const foregroundTexts = $('.firma__foreground-texts');
-  // const doswiadczenie = $('.section__cover-img--doswiadczenie');
-  // foregroundTexts.css('top', `${-foregroundTexts.outerHeight()}px`);
-  // doswiadczenie.height(foregroundTexts.outerHeight());
 }
